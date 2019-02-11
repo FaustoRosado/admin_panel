a962822babc9372a2c869596b05ac6701fbd31e8
Fausto Rosado <faustrosado@gmail.com>
2019-02-10 20:23:28 -0500

    generated course migration & resource

---
+++ app/controllers/courses_controller.rb
@@ -0,0 +1,27 @@
+class CoursesController < ApplicationController
+  
+  def index
+    
+  end
+
+  
+  def show
+  
+  end
+
+  
+  def new
+    
+  end
+
+  
+  def create
+    
+  end
+  
+  def edit
+    
+  end
+  
+  
+end
